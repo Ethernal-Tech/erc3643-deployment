6afe7fa8db1fdbdc2cc427e604237068c7c2d258
oliverbundalo <oliver.bundalo@ethernal.tech>
2025-12-25 17:16:49 +0100

    test for erc3643 getBalances

---
+++ test/erc3643-token.test.ts
@@ -0,0 +1,41 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { deployComplianceFixture } from "./fixtures/deploy-compliance.fixture";
+import ERC3643Token from "../artifacts/contracts/token/ERC3643Token.sol/ERC3643Token.json";
+
+async function deployERC3643Token() {
+  const context = await loadFixture(deployComplianceFixture);
+  const { token } = context.suite;
+  const { aliceWallet, bobWallet, tokenAgent } = context.accounts;
+
+  await token.connect(tokenAgent).mint(aliceWallet, 10)
+  await token.connect(tokenAgent).mint(bobWallet, 20)
+
+  const erc3643Token = await ethers.getContractAt(ERC3643Token.abi, token.target);
+
+  return {
+    ...context,
+    suite: {
+      ...context.suite,
+      erc3643Token,
+    },
+  };
+}
+
+describe("ERC3643 Token", () => {
+  describe(".getBalances", () => {
+    it("should return correct balances", async () => {
+      const context = await loadFixture(deployERC3643Token);
+      const { erc3643Token } = context.suite;
+      const { aliceWallet, bobWallet, anotherWallet } = context.accounts;
+
+      const balances = await erc3643Token.connect(aliceWallet).getBalances([aliceWallet, bobWallet, anotherWallet])
+      expect(balances.length).to.equal(3)
+
+      expect(balances[0]).to.equal(10)
+      expect(balances[1]).to.equal(20)
+      expect(balances[2]).to.equal(0)
+    });
+  });
+});
